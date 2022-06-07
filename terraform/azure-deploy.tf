59a5656685b56e4eb630c894e18a34aa4a921c0f
abhinavneu <abhinav.sreesan@neudesic.com>
2022-06-07 22:39:56 +0530

    updated tf

--- terraform/azure-deploy.tf
+++ terraform/azure-deploy.tf
@@ -6,11 +6,13 @@ resource "azurerm_resource_group" "rg1" {
     Environment = var.environment_tag
   }
 }
+
 #Create KeyVault ID
 resource "random_id" "kvname" {
   byte_length = 5
   prefix      = "keyvault"
 }
+
 #Keyvault Creation
 data "azurerm_client_config" "current" {}
 resource "azurerm_key_vault" "kv1" {
@@ -45,15 +47,27 @@ resource "azurerm_key_vault" "kv1" {
     Environment = var.environment_tag
   }
 }
+
 #Create KeyVault VM password
 resource "random_password" "vmpassword" {
   length  = 20
   special = true
 }
+
 #Create Key Vault Secret
 resource "azurerm_key_vault_secret" "vmpassword" {
   name         = "vmpassword"
   value        = random_password.vmpassword.result
   key_vault_id = azurerm_key_vault.kv1.id
   depends_on   = [azurerm_key_vault.kv1]
+}
+
+#backend
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "neu-dev-data-rg"
+    storage_account_name = "neutstadlsgen2"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
